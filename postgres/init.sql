0a13a42093f298f95ff03fb0dd50c3d8aa69a851
MansorY <a1@MISHLEN-MacBook-Pro-2.local>
2025-03-07 15:29:48 +0500

    feat: update sql init file

--- postgres/init.sql
+++ postgres/init.sql
@@ -32,15 +32,15 @@ create table if not exists public.product
 	updated_at timestamp
 );
 
-drop table if exists public.product_flower_point;
-create table if not exists public.product_flower_point
+drop table if exists public.stock;
+create table if not exists public.stock
 (
+	id bigserial primary key,
 	product_id integer,
 	flower_point_id integer,
     quantity integer NOT NULL DEFAULT 0,
 	created_at timestamp NOT NULL default now(),
-	updated_at timestamp,
-	CONSTRAINT order_product_pkey PRIMARY KEY (flower_point_id, product_id)
+	updated_at timestamp
 );
 
 drop table if exists public.user;
@@ -50,6 +50,8 @@ CREATE TABLE IF NOT EXISTS public.user
 	name text NOT NULL,
 	telegram_id bigint NOT NULL,
 	flower_point_id integer
+	created_at timestamp NOT NULL default now(),
+	updated_at timestamp,
 )
 ;
 
@@ -68,16 +70,16 @@ alter table public.order
 	on delete NO ACTION
 
 ;
-alter table public.product_flower_point drop constraint IF EXISTS fk_product;
-alter table public.product_flower_point 
+alter table public.stock drop constraint IF EXISTS fk_product;
+alter table public.stock 
 	add constraint fk_product
 	foreign key (product_id)
 	references public.product (id)
 	ON DELETE CASCADE
 ;
 
-alter table public.product_flower_point drop constraint IF EXISTS fk_flower_point;
-alter table public.product_flower_point 
+alter table public.stock drop constraint IF EXISTS fk_flower_point;
+alter table public.stock 
 	add constraint fk_flower_point
 	foreign key (flower_point_id)
 	references public.flower_point (id)
@@ -95,9 +97,7 @@ insert into public.flower_point (address) values
 		('Алтын'),
 		('Южка'),
 		('Магнит'),
-		('Пятёрка'),
-		('Южка 2'),
-		('Затон')
+		('Сбербанк')
 ;
 
 insert into public.product (product_name, price) values 
@@ -112,13 +112,14 @@ INSERT INTO public.product_flower_point (product_id, flower_point_id, quantity) 
 	(1, 1, 500),
 	(1, 2, 500),
 	(1, 3, 500),
-	(1, 4, 500),
-	(1, 5, 500),
-	(1, 6, 500)
+	(1, 4, 500)
 ;
 
 INSERT INTO public.user (name, telegram_id, flower_point_id) values
-	('Arseniy', 552489359, 2)
+	 ('Алина', 1272101647, 3),
+	 ('Аня', 1025023173, 4),
+	 ('Элвин', 1076704470, 2)
+	 ('Сергей', 890462786, 1)
 	;
 	
 
